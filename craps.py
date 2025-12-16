6d912109f877a936ea51f0b27417581d7c7faa6c
p-savage <pjslouisville502@gmail.com>
2025-12-15 23:35:52 -0500

    New feature and refactoring
    Major changes that include a refactoring of several blocks into their own modules and the implementation of the new pass line betting feature.

--- craps.py
+++ craps.py
@@ -1,5 +1,8 @@
-import random
+from user_input import*
+from output import*
+from dice_roller import roll_dice
 from game_log import GameLog
+from bets import Bets
 
 class Craps:
     """Run the game"""
@@ -7,6 +10,7 @@ class Craps:
     def __init__(self):
         """Initialize the game"""
         self.log = GameLog()
+        self.bets = Bets(self)
         self.point = 0
         self.puck = "off"
 
@@ -23,24 +27,40 @@ class Craps:
         """Handle the puck off loop"""
         off = True
         while off:
-            prompt = (f"\n\tShooter {self.log.shooter}, the puck is off."+
-                    " Roll to establish a point.\n"+
-                    "\t(Press Enter to roll or Q to quit)")
-            key_press = input(prompt)
-            if key_press.lower() == "q":
-                self.log.update_log()
+            
+            #get a pass line bet
+            pass_line_bet = pass_line_prompt(self.log.shooter)
+            if not pass_line_bet:
+                self.log.update_log(balance=self.bets.balance)
                 self.game_active = False
-                for i in self.log.game_log:
-                    print(i)
+                game_log_message(self.log.game_log)
                 break
-            self.dice_1,self.dice_2 = self.roll_dice()
-            print(f"\nYou rolled {self.dice_1}, {self.dice_2}")
-            total = self.dice_1 + self.dice_2
+            else:
+                self.bets.pass_line_bet = pass_line_bet
+            
+            #prompt the shooter for a come out roll
+            come_out = come_out_prompt(self.log.shooter)
+            if not come_out:
+                self.log.update_log(balance=self.bets.balance)
+                self.game_active = False
+                game_log_message(self.log.game_log)
+                break
+            
+            #call for the roll and increment
+            dice1, dice2 = roll_dice()
+            total = dice1 + dice2
             self.log.inc_roll()
+            
+            #print the total
+            roll_total_message(dice1, dice2)
+            
+            #logic based upon total
             if total in [7, 11]:
-                print(f"Winner! {total} is an automatic win.")
+                automatic_winner_message(total, self.bets.pass_line_bet)
+                self.bets.pass_line_win()
             elif total in [2, 3, 12]:
-                print(f'{total} craps. "Pass" line bets lose.')
+                automatic_loser_message(total, self.bets.pass_line_bet)
+                self.bets.pass_line_loss()
             else:
                 self.point = total
                 off = False
@@ -50,46 +70,52 @@ class Craps:
         """Handle the puck on loop"""
         on = True
         while on:
-            prompt = (
-            f"\n\tThe point is {self.point}. Hit the point again before rolling a 7.\n"+
-            "\t(Press Enter to roll or Q to quit)"
-            )
-            key_press = input(prompt)
-            if key_press.lower() == 'q':
-                self.log.update_log()
+            
+            #prompt to keep rolling
+            roll_again = roll_again_prompt(self.point)
+            if not roll_again:
+                self.log.update_log(balance=self.bets.balance)
                 self.game_active = False
-                for i in self.log.game_log:
-                    print(i)
+                game_log_message(self.log.game_log)
                 break
-            self.dice_1,self.dice_2 = self.roll_dice()
-            print(f"\nYou rolled {self.dice_1}, {self.dice_2}")
-            total = self.dice_1 + self.dice_2
+            
+            #call for the roll and increment
+            dice1, dice2 = roll_dice()
+            total = dice1 + dice2
             self.log.inc_roll()
+           
+            #print the total
+            roll_total_message(dice1, dice2)
+            
+            #logic based upon total
             if total == 7:
-                print("7 craps. New shooter.")
-                self.log.update_log()
+                #print output
+                seven_out_message(self.bets.pass_line_bet)
+                #process bet outcome
+                self.bets.pass_line_loss()
+                #update log
+                self.log.update_log(balance=self.bets.balance)
+                #increment shooter and reset appropriate values
                 self.log.inc_shooter()
+                self.bets.reset_balance()
                 self.log.reset_rolls()
                 self.log.reset_points_hit()
                 self.point = 0
                 on = False
                 self.puck = "off"
             elif total == self.point:
-                print(
-                f"{total} is a Winner! Roll again to establish a new point."
-                    )
+                #print output
+                point_hit_message(total, self.bets.pass_line_bet)
+                #process bet outcome
+                self.bets.pass_line_win()
+                #update log
                 self.log.inc_points()
+                #reset values and flags
                 self.point = 0
                 on = False
                 self.puck = "off"
             else:
-                print(f"{total} is not the point.")
-
-    def roll_dice(self):
-        """Simulate rolling two dice."""
-        self.dice_1 = random.randint(1, 6)
-        self.dice_2 = random.randint(1, 6)
-        return self.dice_1, self.dice_2
+                trivial_total_message(total)
         
 c = Craps()
 c.run_game()
